8d1bfbe783854878013dd1450b6691f1a9fd0d06
Krishanu <iamkrishanu@Krishanus-MacBook-Air.local>
2026-06-06 20:44:59 +0530

    Auth service cors removed

--- AuthService/src/main/java/com/example/demo1/configuration/AuthConfiguration.java
+++ AuthService/src/main/java/com/example/demo1/configuration/AuthConfiguration.java
@@ -1,4 +1,5 @@
 package com.example.demo1.configuration;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -27,82 +28,65 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
 import com.example.demo1.exception.AuthAccessDeniedHandler;
+
 @Configuration
 @EnableWebSecurity
 public class AuthConfiguration {
-	
+
 	@Lazy
 	@Autowired
 	private UserDetailsImpl userDetails;
-	
-	
-	
-	
-    @Bean
-    public RedisTemplate<String, AuthDB> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, AuthDB> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
 
-        Jackson2JsonRedisSerializer<AuthDB> serializer = new Jackson2JsonRedisSerializer<>(AuthDB.class);
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.activateDefaultTyping(mapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
-        serializer.setObjectMapper(mapper);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(serializer);
-        template.afterPropertiesSet();
-        return template;
-    }
-
-	
-	
-	
 	@Bean
-   public WebMvcConfigurer corsConfigurer() {
-       return new WebMvcConfigurer() {
-           @Override
-           public void addCorsMappings(CorsRegistry registry) {
-               registry.addMapping("/**")
-                       .allowedOrigins("http://localhost:4200")  // your Angular app URL
-                       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                       .allowedHeaders("*")
-                       .allowCredentials(true);
-           }
-       };
-   }
-	
-	@Bean	
+	public RedisTemplate<String, AuthDB> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, AuthDB> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		Jackson2JsonRedisSerializer<AuthDB> serializer = new Jackson2JsonRedisSerializer<>(AuthDB.class);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		mapper.activateDefaultTyping(mapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+		serializer.setObjectMapper(mapper);
+
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(serializer);
+		template.afterPropertiesSet();
+		return template;
+	}
+
+	@Bean
 	public PasswordEncoder encodePassword() {
 		return new BCryptPasswordEncoder();
-		
+
 	}
+
 	@Bean
 	public AuthenticationManager manager(AuthenticationConfiguration config) throws Exception {
 		return config.getAuthenticationManager();
 	}
+
 	@Bean
 	public SecurityFilterChain secureHttps(HttpSecurity http) throws Exception {
 		try {
-			http.csrf().disable().authorizeHttpRequests(auth->auth.requestMatchers("/auth-service/**").permitAll()
+			http.csrf().disable().authorizeHttpRequests(auth -> auth.requestMatchers("/auth-service/**").permitAll()
 					.anyRequest().authenticated())
-			.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.exceptionHandling(ex-> ex.accessDeniedHandler(new AuthAccessDeniedHandler()));
+					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+					.exceptionHandling(ex -> ex.accessDeniedHandler(new AuthAccessDeniedHandler()));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return http.build();
 	}
-	
+
 	@Bean
 	public AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetails);
 		provider.setPasswordEncoder(encodePassword());
 		return provider;
 	}
-//	
+	//
 
 }
