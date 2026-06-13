c86a4fe67b5cc356655a38d19b279f03620a5d7d
Krishanu <iamkrishanu@Krishanus-MacBook-Air.local>
2026-06-13 16:19:51 +0530

    ai service added for chat

--- ai-service/src/main/java/ai/service/ai_service/controller/AiController.java
+++ ai-service/src/main/java/ai/service/ai_service/controller/AiController.java
@@ -2,14 +2,12 @@ package ai.service.ai_service.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ai.service.ai_service.ToolCalling.AppointmentAgent;
 
 @RestController
-@RequestMapping("/ai")
 public class AiController {
 
     private final AppointmentAgent agent;
